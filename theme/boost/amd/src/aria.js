80a00558a91cc6a250aa26148320b2fa67af3e9c
Trisha Milan <trishamilan@catalyst-au.net>
2024-10-16 16:58:34 +1100

    MDL-82625 navigation: Add disableactive flag to combobox widget
    - Introduced disableactive flag to prevent setting active state on combobox items when a page redirect occurs.
    This resolves the issue where incorrect checkmarks remain after using the browser's back button due to bfcache.
    - Added Behat test cases to verify correct checkmark behaviour in tertiary navigation when navigating and using
    the browser's back/forward buttons.

--- theme/boost/amd/src/aria.js
+++ theme/boost/amd/src/aria.js
@@ -372,12 +372,21 @@ const comboboxFix = () => {
         const listbox = option.closest('[role="listbox"]');
         const oldSelectedOption = listbox.querySelector('[role="option"][aria-selected="true"]');
 
-        if (oldSelectedOption != option) {
-            if (oldSelectedOption) {
-                oldSelectedOption.removeAttribute('aria-selected');
-            }
-            option.setAttribute('aria-selected', 'true');
+        if (option === oldSelectedOption) {
+            processSelectedOption(combobox, option.dataset.value);
+            return;
         }
+
+        processSelectedOption(combobox, option.dataset.value);
+
+        if (option.dataset.disableactive) {
+            return;
+        }
+
+        if (oldSelectedOption) {
+            oldSelectedOption.removeAttribute('aria-selected');
+        }
+        option.setAttribute('aria-selected', 'true');
 
         if (combobox.hasAttribute('value')) {
             combobox.value = option.dataset.shortText || option.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
@@ -389,13 +398,26 @@ const comboboxFix = () => {
                 combobox.textContent = option.dataset.shortText || option.textContent;
             }
         }
+    };
 
-        if (combobox.dataset.inputElement) {
-            const inputElement = document.getElementById(combobox.dataset.inputElement);
-            if (inputElement && (inputElement.value != option.dataset.value)) {
-                inputElement.value = option.dataset.value;
-                inputElement.dispatchEvent(new Event('change', {bubbles: true}));
-            }
+    const processSelectedOption = (combobox, optionvalue) => {
+        const useCustomEvent = combobox.dataset.actiontype === 'event';
+        const event = useCustomEvent ? new CustomEvent('change', {
+            bubbles: true, detail: {value: optionvalue}}) : new Event('change', {bubbles: true});
+        const element = useCustomEvent ? combobox : document.getElementById(combobox.dataset.inputElement);
+
+        if (!element) {
+            return;
+        }
+
+        if (useCustomEvent) {
+            element.dispatchEvent(event);
+            return;
+        }
+
+        if (element.value != optionvalue) {
+            element.value = optionvalue;
+            element.dispatchEvent(event);
         }
     };
 };
