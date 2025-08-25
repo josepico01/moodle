a2472243a735c0c6ac3bedf3fbd2b8ada963576e
Jose Pico <jose.pico@monash.edu>
2025-08-25 16:27:25 +1000

    MDL-86422 quiz: Prevents race conditions in adhoc task scheduling
    Adds a lock to prevent race conditions when scheduling adhoc tasks,
    ensuring that only one instance of a task is scheduled at a time.
    Move the transaction commit to ensure that the database
    changes occur before firing the events.

--- mod/quiz/classes/quiz_attempt.php
+++ mod/quiz/classes/quiz_attempt.php
@@ -1859,13 +1859,13 @@ class quiz_attempt {
 
         $DB->update_record('quiz_attempts', $this->attempt);
 
+        $transaction->allow_commit();
         if (!$this->is_preview()) {
             // Trigger event.
             $this->fire_state_transition_event('\mod_quiz\event\attempt_submitted', $timestamp, $studentisonline);
             \core\hook\manager::get_instance()->dispatch(new attempt_state_changed($originalattempt, $this->attempt));
         }
 
-        $transaction->allow_commit();
     }
 
     /**
@@ -1896,6 +1896,7 @@ class quiz_attempt {
 
         $DB->update_record('quiz_attempts', $this->attempt);
 
+        $transaction->allow_commit();
         if (!$this->is_preview()) {
             $this->recompute_final_grade();
 
@@ -1907,7 +1908,6 @@ class quiz_attempt {
             $this->get_access_manager($timestamp)->current_attempt_finished();
         }
 
-        $transaction->allow_commit();
     }
 
     /**
@@ -1948,10 +1948,10 @@ class quiz_attempt {
         $this->attempt->timecheckstate = $timestamp;
         $DB->update_record('quiz_attempts', $this->attempt);
 
+        $transaction->allow_commit();
         $this->fire_state_transition_event('\mod_quiz\event\attempt_becameoverdue', $timestamp, $studentisonline);
 
         di::get(hook\manager::class)->dispatch(new attempt_state_changed($originalattempt, $this->attempt));
-        $transaction->allow_commit();
 
         quiz_send_overdue_message($this);
     }
@@ -1972,11 +1972,11 @@ class quiz_attempt {
         $this->attempt->timecheckstate = null;
         $DB->update_record('quiz_attempts', $this->attempt);
 
+        $transaction->allow_commit();
         $this->fire_state_transition_event('\mod_quiz\event\attempt_abandoned', $timestamp, $studentisonline);
 
         di::get(hook\manager::class)->dispatch(new attempt_state_changed($originalattempt, $this->attempt));
 
-        $transaction->allow_commit();
     }
 
     /**
@@ -2003,6 +2003,7 @@ class quiz_attempt {
         $this->attempt->timecheckstate = null;
         $DB->update_record('quiz_attempts', $this->attempt);
 
+        $transaction->allow_commit();
         $this->fire_state_transition_event('\mod_quiz\event\attempt_reopened', $timestamp, false);
 
         di::get(hook\manager::class)->dispatch(new attempt_state_changed($originalattempt, $this->attempt));
@@ -2012,7 +2013,6 @@ class quiz_attempt {
             $this->process_grade_submission($timeclose);
         }
 
-        $transaction->allow_commit();
     }
 
     /**
